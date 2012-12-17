4dadf6d266ec41688775f318e28c1d7979cee350
Yarek <bsb_yarek@bk.ru>
2012-12-17 20:37:12 +0200

    qas

---
+++ test_projects/Domain/EFDdContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using Domain.Entities;
+
+namespace Domain
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext(string connectionString)
+        {
+            Database.Connection.ConnectionString = connectionString;
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
