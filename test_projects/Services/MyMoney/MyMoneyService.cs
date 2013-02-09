c8f8ad505f28e45317a0de7bfba8a4a92331e919
Yarek <bsb_yarek@bk.ru>
2013-02-09 09:52:39 +0200

    commit

--- test_projects/Services/MyMoney/MyMoneyService.cs
+++ test_projects/Services/MyMoney/MyMoneyService.cs
@@ -1,15 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Objects;
 using System.Linq;
 using System.Text;
+using Domain.MyMoney;
 
 namespace Services.MyMoney
 {
     public interface IMyMoneyInterface
     {
-        
+        JournalPayment GetJournalPaymentById(int id);
+        void AddJournalPayment(JournalPayment journalPayment);
+        Balance GetBalanceByDateTime(DateTime dateTime);
+        void AddBalance(Balance balance);
+        OperationType GetOperationTypeById(int id);
+        void AddOperationType(OperationType operationType);
+        Prioritet GetPrioritetById(int id);
+        void AddPrioritet(Prioritet prioritet);
+
+        List<JournalPayment> GetAllJournalPaymentsByDate(DateTime dateTime);
+
     }
     public class MyMoneyService:BaseService,IMyMoneyInterface
     {
+
+        public JournalPayment GetJournalPaymentById(int id)
+        {
+            return Service.MyMoneyContext.JournalPayments.Find(id);
+        }
+
+        public void AddJournalPayment(JournalPayment journalPayment)
+        {
+            if (journalPayment!=null) Service.MyMoneyContext.JournalPayments.Add(journalPayment);
+            Service.Commit();
+        }
+
+        public Balance GetBalanceByDateTime(DateTime dateTime)
+        {
+            return Service.MyMoneyContext.Balance.FirstOrDefault(_ => _.DateTime == dateTime);
+        }
+
+        public void AddBalance(Balance balance)
+        {
+            if (balance != null) Service.MyMoneyContext.Balance.Add(balance);
+            Service.Commit();
+        }
+
+        public OperationType GetOperationTypeById(int id)
+        {
+            return Service.MyMoneyContext.OperationTypes.Find(id);
+        }
+
+        public void AddOperationType(OperationType operationType)
+        {
+            if (operationType != null) Service.MyMoneyContext.OperationTypes.Add(operationType);
+            Service.Commit();
+        }
+        
+        public Prioritet GetPrioritetById(int id)
+        {
+            return Service.MyMoneyContext.Prioritets.Find(id);
+        }
+
+        public void AddPrioritet(Prioritet prioritet)
+        {
+            if (prioritet != null) Service.MyMoneyContext.Prioritets.Add(prioritet);
+        }
+
+        public List<JournalPayment> GetAllJournalPaymentsByDate(DateTime dateTime)
+        {
+            return (from jp in Service.MyMoneyContext.JournalPayments
+                    where EntityFunctions.TruncateTime(jp.DateTime) == dateTime.Date
+                    select jp).ToList();
+        }
     }
 }
---
+++ test_projects/UsingEF/Controllers/MyMoneyController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.MyMoney;
+
+namespace UsingEF.Controllers
+{
+    public class MyMoneyController : BaseController
+    {
+
+        public ActionResult Index()
+        {
+            using (var service = GetService())
+            {
+                var model = service.MyMoneyService.GetAllJournalPaymentsByDate(DateTime.Now);
+            return View(model);
+            }
+            
+        }
+
+        public ActionResult AddNewPayment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNewPayment(JournalPayment journalPayment)
+        {
+            using (var service = GetService())
+            {
+                if (ModelState.IsValid)
+                {
+                    var jp = new JournalPayment()
+                        {
+                            Amount = journalPayment.Amount,
+                            Currency = journalPayment.Currency,
+                            DateTime = DateTime.Now,
+                            Description = "First",
+                            OperationTypeId = 1,
+                            PrioritetId = 1
+                        };
+                    service.MyMoneyService.AddJournalPayment(jp);
+                    //service.MyMoneyContext.JournalPayments.Add(jp);
+                    //service.MyMoneyContext.SaveChanges();
+                }
+                //service.MyMoneyService.AddJournalPayment(journalPayment);
+            }
+
+           return RedirectToAction("Index");
+        }
+
+    }
+}
